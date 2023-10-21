425ba92863ec635bd41ac53bc891986fe15b5438
BlockMetro <147730647+BlockMetro@users.noreply.github.com>
2023-10-21 16:44:54 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from disutils import setup
+
+setup(
+    name = 'blockmetro',
+    packages = ['blockmetro'],
+    version = '1.0',
+    lisence = 'MIT',
+    description = '',
+    author = 'BlockMetro LLC',
+    author_email = 'admin@blockmetro.io',
+    url = 'https://github.com/BlockMetro/blockmetro-python',
+    download_url = 'https://github.com/BlockMetro/blockmetro-python',
+    keywords = ['cardano', 'api'],
+    install_requires = [],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Licence :: OSI Approved :: MIT Licence',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
